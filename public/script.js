36c88e48e77b4d13a9aec08f250ca71b5999f459
IamFloopy <xfilipxfilipx@gmail.com>
2021-10-19 19:03:02 +0200

    heroku

--- public/script.js
+++ public/script.js
@@ -7,10 +7,9 @@ const nickname = document.getElementById("nickname");
 const activeGrid = document.getElementById('active-grid')
 
 
-const myPeer = new Peer(undefined, {
+const myPeer = new Peer("someid", {
   secure: true,
   host: 'me-and-the-homies.herokuapp.com',
-  port: '443'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
